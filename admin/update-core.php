daf8cd88b6145059a5dacc057225149da1c77a63
Andre.my.id <GitHub@andre.my.id>
2014-05-16 03:30:21 +0700

    Create update-core.php

---
+++ admin/update-core.php
@@ -0,0 +1,3 @@
+
+		echo '<div class="updated inline"><p>';
+		_e('<strong>Penting:</strong> sebelum memperbarui SISTEM, silakan cadangkan basis data dan berkas-berkas Anda. Untuk bantuan dan suport pembaruan SISTEM, silahkan hubungi Andre di telpon: (62) 8979 679967 / PIN BBM: 74F3 52BC.');
